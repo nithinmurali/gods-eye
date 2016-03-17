4a3365948bf1f81ea474cd7e93b9655851f64cd0
nithinmurali <imnmfotmal@gmail.com>
2016-03-17 13:32:51 +0530

    small edits

--- main.cpp
+++ main.cpp
@@ -19,75 +19,6 @@
 using namespace cv;
 using namespace std;
 
-void balance_white(cv::Mat mat) {
-  double discard_ratio = 0.012;
-  int hists[3][256];
-  memset(hists, 0, 3*256*sizeof(int));
-
-  for (int y = 0; y < mat.rows; ++y) {
-    uchar* ptr = mat.ptr<uchar>(y);
-    for (int x = 0; x < mat.cols; ++x) {
-      for (int j = 0; j < 3; ++j) {
-        hists[j][ptr[x * 3 + j]] += 1;
-      }
-    }
-  }
-
-  //check if you wanna strech red
-  int mean=0,N=0,min=-1,bin=0,start=3;
-  for (int j = 255; j > -1; --j) {
-      bin = hists[2][j];
-      mean += bin*j;
-      N += bin;
-      if(min==-1 && bin>10)
-        min=j;
-    //cout<<j<<":"<<bin<<',';
-    }
-  cout<<"\n";
-  mean /= N;
-  cout<<"mean:"<<mean<<" min:"<<min<<endl;
-  if(mean < 25 && min < 50 )
-    start=2;
-
-  // cumulative hist
-  int total = mat.cols*mat.rows;
-  int vmin[3], vmax[3];
-  for (int i = 0; i < start; ++i) {
-
-    for (int j = 0; j < 255; ++j) {
-      hists[i][j + 1] += hists[i][j];
-    }
-    vmin[i] = 0;
-    vmax[i] = 255;
-    while (hists[i][vmin[i]] < discard_ratio * total)
-      vmin[i] += 1;
-    while (hists[i][vmax[i]] > (1 - discard_ratio) * total)
-      vmax[i] -= 1;
-    if (vmax[i] < 255 - 1)
-      vmax[i] += 1;
-  }
-
-
-  for (int y = 0; y < mat.rows; ++y) {
-    uchar* ptr = mat.ptr<uchar>(y);
-    for (int x = 0; x < mat.cols; ++x) {
-      
-      for (int j = 0; j < start; ++j) {
-        
-        int val = ptr[x * 3 + j];
-        if (val < vmin[j])
-          val = vmin[j];
-        if (val > vmax[j])
-          val = vmax[j];
-        ptr[x * 3 + j] = static_cast<uchar>((val - vmin[j]) * 255.0 / (vmax[j] - vmin[j]));
-      }
-    
-    }
-  }
-
-}
-
-
 
 void balance_white1(cv::Mat &mat) 
 {
@@ -208,14 +139,17 @@ void inten(Mat image)
     var = sum/totalCount;
 
     cout<<"mean intensity: "<<mean<<" var"<<var<<endl;
-
 }
 
  int main( int argc, char** argv )
 {
     int type = 1;//img
     Mat imgOriginal;
-    VideoCapture cap(argv[1]); // try capture the video
+    VideoCapture cap; // try capture the video
+    if(argv[1][0] == '0' )
+      cap.open(0);
+    else
+      cap.open(argv[1]);
 
     Mat input, image, dst;
     input.create(240,320,CV_8UC(3));
@@ -235,17 +169,13 @@ void inten(Mat image)
         imgOriginal = imread(argv[1], CV_LOAD_IMAGE_UNCHANGED);
     }
 
-    int run = atoi(argv[2]);
-
     int i =1;
     while (true)
     {
-      
         int k = waitKey(FPS);
         //cout<<k<<endl;
         if (type == 2 && i == 1)
         {
-
             bool bSuccess = cap.read(imgOriginal); // read a new frame from video
             if (!bSuccess) //if not success, break loop
             {
@@ -277,8 +207,8 @@ void inten(Mat image)
         /*cv::cvtColor (im1,im1,CV_BGR2YCrCb);
         vector <Mat> channels;
         cv::split(im1,channels);
-        imshow("intensity",channels[0]);
-*/
+        imshow("intensity",channels[0]);*/
+
         balance_white1(input);
         imshow("white balence",input);
 
@@ -296,7 +226,6 @@ void inten(Mat image)
         {
             i=1;
         }
-        
     }
 
    return 0;
